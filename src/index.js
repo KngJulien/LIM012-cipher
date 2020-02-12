3bdd6bb6138d266ad0ac48a0797dd0af54621c44
KngJulien <aguilar_urrunaga@hotmail.com>
2020-02-12 11:12:26 -0500

    Actualizacion de archivo HTML, actualizacion archivo index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+codis.addEventListener("click")
+  const codis$ = document.getElementById('cipher');
+  codis$.style.display ='block';
+
+decodis.addEventListener("click")
+ const decodis$ = document.getElementById('decipher');
+    decipher.style.display ='block';
+
+
 import cipher from './cipher.js';
 
 console.log(cipher);
